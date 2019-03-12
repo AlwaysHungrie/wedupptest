8b87b332e30576ba20dbefb4068aba38d46c00df
AlwaysHungrie <dhairyashah98@gmail.com>
2019-03-13 03:29:33 +0530

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const server = require('http').Server(express);
+const io = require('socket.io')(server);
+const path = require('path');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const app = express();
+
+app.use('/',express.static(path.join(__dirname, 'dist/wedupptest')));
+
+app.get('/weduppapi',async (req,res)=>{
+
+	let n = req.query.n;
+
+	let textbyline = await fetch("https://gitlab.com/snippets/1824628/raw")
+    .then(res => res.text())
+    .then(body => readFile(body));
+
+
+    let fre = await getnfreq(textbyline,n);
+    //console.log(fre)
+    //.then(textByLine => getnfreq(textByLine,n)) 
+   	res.send(fre);
+});	
+
+const getnfreq = (async(text,n)=>{
+	//console.log(text);
+	let frequencies = new Object();
+	console.log(n);
+	for(let word in text){
+		if(text[word] && !([ "the", "a", "of", "and", "in", "is", "to"].includes(text[word]))){
+			if(frequencies.hasOwnProperty(text[word])){
+				//console.log(text[word])
+				frequencies[text[word]] += 1;
+			}else{
+				//console.log(text[word])
+				let p = text[word]
+				frequencies[p] = 1;
+			}
+		}
+	}
+
+
+	function sortProperties(obj){
+		var sortable=[];
+		for(var key in obj)
+			if(obj.hasOwnProperty(key))
+				sortable.push([key, obj[key]]); 
+		sortable.sort(function(a, b){
+		  return b[1]-a[1];
+		});
+		return sortable; 
+	}
+
+	var sortedFreq = sortProperties(frequencies);
+	var results = [];
+
+	if(n>0){
+		for(var i = 0; i < Math.min(n,sortedFreq.length)	; i++){
+  			results.push(sortedFreq[i][0]);
+		}
+	}
+
+	
+
+	return results;
+
+})
+
+
+const readFile = (async (body)=>{
+	var text = body;
+	var textByLine = text.replace( /[\n.,‘’'"]/g, " " ).toLowerCase().split(" ");
+	return new Promise((resolve,reject)=>{
+		//console.log(textByLine);
+		resolve(textByLine);
+	})
+})
+
+app.listen(3000,()=>{
+	console.log("server listening on port: "+3000);
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient){}
   title = 'wedupptest';
+  illegalArr = [ "the", "a", "of", "and", "in", "is", "to"]
+  words = [];
+  nfreq = 0;
+
+  onKey(value: number) {
+    this.nfreq = value;
+  }
+
+	getWords(){
+		return this.http.get('/weduppapi?n='+this.nfreq);
+	}
+
+	getwords(){
+		this.getWords().subscribe((res : any[])=>{
+			this.words = res;
+		});
+	}  
+
+	ngOnInit(){
+	}
+
 }
